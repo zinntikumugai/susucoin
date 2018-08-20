3cdec9d2384f6dd0c68b08d1dd0699a47ea0e765
sd8 <sd8@disroot.org>
2018-08-20 09:38:26 +0200

    Make transactions with up to 16 OP_RETURN that are up to 512 bytes standard, that is enable their relaying by default.

--- src/script/standard.h
+++ src/script/standard.h
@@ -28,10 +28,10 @@ public:
 };
 
 /**
- * Default setting for nMaxDatacarrierBytes. 80 bytes of data, +1 for OP_RETURN,
- * +2 for the pushdata opcodes.
+ * Default setting for nMaxDatacarrierBytes. 512 bytes of data, +1 for OP_RETURN,
+ * +3 for the pushdata opcodes.
  */
-static const unsigned int MAX_OP_RETURN_RELAY = 83;
+static const unsigned int MAX_OP_RETURN_RELAY = 516;
 
 /**
  * A data carrying output is an unspendable output containing data. The script
